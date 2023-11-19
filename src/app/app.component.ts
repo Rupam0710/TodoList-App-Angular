412c65911f51294f565517c66d3680bebd961c26
Rupam <rupampal48@gmail.com>
2023-11-19 23:00:55 +0530

    1st commit - todolist angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TodoList-App-Angular';
+  newTask = '';
+  tasks = [];
+  completedTasks = [];
+
+  public addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push({ name: this.newTask.trim(), completed: false });
+      this.newTask = '';
+    }
+  }
+
+  public editTask(index: number) {
+    this.tasks[index].name = prompt('Edit Task', this.tasks[index].name);
+  }
+
+  public deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  public markComplete(index: number) {
+    this.tasks[index].completed = true;
+    this.completedTasks.push(this.tasks.splice(index, 1)[0]);
+
+  }
 }
